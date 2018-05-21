3b828c96199214dc9e2a1289e6574849e50f28f5
Riky Perdana <perdanariky@gmail.com>
2018-05-21 11:06:32 +0700

    first push inap

---
+++ inap/programs/server/app/global-imports.js
@@ -0,0 +1,26 @@
+/* Imports for global scope */
+
+MongoInternals = Package.mongo.MongoInternals;
+Mongo = Package.mongo.Mongo;
+ReactiveVar = Package['reactive-var'].ReactiveVar;
+Tracker = Package.tracker.Tracker;
+Deps = Package.tracker.Deps;
+ECMAScript = Package.ecmascript.ECMAScript;
+prelude = Package['zenkogu:livescript-compiler'].prelude;
+lodash = Package['stevezhu:lodash'].lodash;
+_ = Package.underscore._;
+moment = Package['momentjs:moment'].moment;
+Meteor = Package.meteor.Meteor;
+global = Package.meteor.global;
+meteorEnv = Package.meteor.meteorEnv;
+WebApp = Package.webapp.WebApp;
+WebAppInternals = Package.webapp.WebAppInternals;
+main = Package.webapp.main;
+DDP = Package['ddp-client'].DDP;
+DDPServer = Package['ddp-server'].DDPServer;
+LaunchScreen = Package['launch-screen'].LaunchScreen;
+meteorInstall = Package.modules.meteorInstall;
+meteorBabelHelpers = Package['babel-runtime'].meteorBabelHelpers;
+Promise = Package.promise.Promise;
+Autoupdate = Package.autoupdate.Autoupdate;
+
---
+++ inap/programs/server/packages/michalvalasek_jquery-marquee.js
@@ -0,0 +1,24 @@
+(function () {
+
+/* Imports */
+var Meteor = Package.meteor.Meteor;
+var global = Package.meteor.global;
+var meteorEnv = Package.meteor.meteorEnv;
+
+(function(){
+
+///////////////////////////////////////////////////////////////////////
+//                                                                   //
+// packages/michalvalasek_jquery-marquee/packages/michalvalasek_jque //
+//                                                                   //
+///////////////////////////////////////////////////////////////////////
+                                                                     //
+///////////////////////////////////////////////////////////////////////
+
+}).call(this);
+
+
+/* Exports */
+Package._define("michalvalasek:jquery-marquee");
+
+})();
